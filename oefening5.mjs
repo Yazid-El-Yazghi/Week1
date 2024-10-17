95d1548f4ad9b62e70db96b63963618f588f3689
Yazid-El-Yazghi <yaya250@live.fr>
2024-10-17 11:25:34 +0200

    Update oefening5.mjs
    einde oefening5

--- oefening5.mjs
+++ oefening5.mjs
@@ -12,15 +12,25 @@ let naam = await userInput.question('Schrijf je naam in: ');
 begroeting(naam); */
 
 /* //Oppervlakes
-function Oppervlakte(vorm, straal, basis, hoogte, zijde1, zijde2, zijde){
+let vorm = await userInput.question('Geef de naam van het vorm in (cirkel/driehoek/rechthoek/vierkant): ')
+
+Oppervlakte(vorm);
+
+async function Oppervlakte(vorm){
     let Oppervlakte;
     if(vorm == 'cirkel'){
+        let straal = parseFloat(await userInput.question('Geef de straal in: '));
         Oppervlakte = straal*straal*Math.PI;
     }else if(vorm == 'driehoek'){
+        let basis = parseFloat(await userInput.question('Geef de basis in: '));
+        let hoogte = parseFloat(await userInput.question('Geef het hoogte in: '));
         Oppervlakte = (basis*hoogte)/2;
     }else if(vorm == 'rechthoek'){
+        let zijde1 = parseFloat(await userInput.question('Geef de eerste zijde in: '));
+        let zijde2 = parseFloat(await userInput.question('Geef de tweede zijde in: '));
         Oppervlakte = zijde1*zijde2;
     }else if(vorm == 'vierkant'){
+        let zijde = parseFloat(await userInput.question('Geef een zijde in: '));
         Oppervlakte = zijde*zijde;
     }else{
         console.log('Wrong input!');
@@ -30,7 +40,7 @@ function Oppervlakte(vorm, straal, basis, hoogte, zijde1, zijde2, zijde){
     process.exit();
 } */
 
-//Temperatuurconverter
+/* //Temperatuurconverter
 let temperatuur = parseFloat (await userInput.question("geef een temperatuur in: "));
 let choice = await userInput.question("is de temperatuur in celcius? [y/n]");
 let isCelcius;
@@ -52,5 +62,101 @@ function Temperatuurconverter(temperatuur, isCelcius){
         console.log('Je temperatuur in celcius is: '+temperatuur);
         process.exit();
     }
+} */
+
+/* //Factorial
+let factor = parseFloat(await userInput.question('Geef een getal in: '))
+
+Factorial(factor);
+
+function Factorial(factor){
+    let fac = 1;
+    for(let i = 1; i <= factor; i++){
+        fac *= i;
+    }
+    console.log('De factorial van '+factor+' is '+fac);
+    process.exit();
+} */
+
+/* //Hoger/lager v3
+let getal = parseFloat(await userInput.question('Raad het getal tussen 1 en 10: '));
+
+HogerOfLager(getal);
+
+async function HogerOfLager(getal) {
+    let cpt = 1;
+    let random = Math.floor(Math.random() * 10)+1;
+    while(cpt < 3){
+        if(getal != random){
+            if(getal < random){
+                console.log('Het getal is hoger dan '+getal);
+                getal = parseFloat(await userInput.question('Raad het getal tussen 1 en 10: '));
+                cpt++;
+            }else{
+                console.log('Het getal is lager dan '+getal);
+                getal = parseFloat(await userInput.question('Raad het getal tussen 1 en 10: '));
+                cpt++;
+            }
+        }else{
+            console.log('Prima! je heeft het juiste getal gevonden!');
+            console.log('Het getal was: '+getal);
+            process.exit();
+        }
+    }
+    console.log('GAME OVER');
+    console.log('Helaas het getal was: '+random);
+    process.exit();
+} */
+
+/* //Perfecte getallen
+let perfect = parseFloat(await userInput.question('Geef een getal tussen 1 en 100: '));
+
+isParfect(perfect);
+
+function isParfect(perfect) {
+    let teller = 0;
+
+    if(perfect > 100 && perfect < 1){
+        console.log('Het getal ligt niet tussen 1 en 100');
+        process.exit();
+    }
+
+    for (let index = 1; index < perfect; index++) {
+        if(perfect %index == 0){
+            teller += index;
+        }
+    }
+
+    if(teller == perfect){
+        console.log('Het getal '+perfect+' is een perfecte getal');
+    }else{
+        console.log('Het getal '+perfect+' is geen perfecte getal');
+    }
+    process.exit();
+} */
+
+//Dagen in de maand
+let maand = await userInput.question('Geef een maand in: ');
+let jaar = parseFloat(await userInput.question('Geef een jaar in: '));
+
+DagenInDeMaand(maand, jaar);
+
+function DagenInDeMaand(maand, jaar){
+    let schrikeljaar = false;
+
+    if(jaar%4 == 0 && (jaar%100 != 0 || jaar%400 == 0)){
+        schrikeljaar = true;
+    }
+
+    if((maand == 'Januarie')||(maand == 'Maart')||(maand == 'Mei')||(maand == 'Julie')||(maand == 'Augustus')||(maand == 'Oktober')||(maand == 'December')){
+        console.log('Het maand '+maand+' heeft 31 dagen');
+    }else if((maand == 'April')||(maand == 'Junie')||(maand == 'September')||(maand == 'November')){
+        console.log('Het maand '+maand+' heeft 30 dagen');
+    }else if(maand == 'Februarie' && schrikeljaar == true){
+        console.log('Het maand '+maand+' heeft 29 dagen');
+    }else{
+        console.log('Het maand '+maand+' heeft 28 dagen');
+    }
+    process.exit();
 }
 
